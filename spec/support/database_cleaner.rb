eac2ca625fd7d276e0bc1a964c25d805eaee5012
Alessandro Desantis <desa.alessandro@gmail.com>
2016-04-15 20:05:54 +0200

    Configure testing tools

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,19 @@
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation,
+      except: %w[spatial_ref_sys schema_migrations]
+    )
+  end
+
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation,
+      { except: %w[spatial_ref_sys schema_migrations] }
+  end
+
+  config.before(:each) { DatabaseCleaner.start }
+  config.append_after(:each) { DatabaseCleaner.clean }
+end
