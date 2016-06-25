9a384f72b0e99b0d0010af2b2a5b8b220bf54cf4
Alessandro Desantis <desa.alessandro@gmail.com>
2016-06-25 15:48:09 +0200

    Update NotificationDeliverer test

--- spec/hertz/notification_deliverer_spec.rb
+++ spec/hertz/notification_deliverer_spec.rb
@@ -3,16 +3,33 @@ module Hertz
   RSpec.describe NotificationDeliverer do
     subject { described_class }
 
+    let(:test_courier) do
+      Class.new do
+        def deliver_notification(notification)
+        end
+      end.new
+    end
+
     before(:each) do
-      class TestNotification < Notification
-        deliver_by :test
-      end
+      allow(Hertz).to receive(:build_courier)
+        .with(:test)
+        .and_return(test_courier)
     end
 
     describe '#deliver' do
-      let(:notification) { TestNotification.new }
+      let(:notification) do
+        Class.new(TestNotification) do
+          deliver_by :test
+        end.new
+      end
 
-      it 'delivers the notification through the couriers'
+      it 'delivers the notification through the couriers' do
+        expect(test_courier).to receive(:deliver_notification)
+          .once
+          .with(notification)
+
+        subject.deliver(notification)
+      end
     end
   end
 end
