aecb439256d5de939475ad1717d09ae19545feec
Alessandro Desantis <desa.alessandro@gmail.com>
2016-06-25 16:16:25 +0200

    Fix test order bug

--- spec/hertz/hertz_spec.rb
+++ spec/hertz/hertz_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 RSpec.describe Hertz do
   describe '.configure' do
+    after(:each) { Hertz.common_couriers = [] }
+
     it 'configures the module' do
       expect {
         described_class.configure do |config|
