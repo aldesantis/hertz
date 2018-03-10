209182968a0913a7af7d0d8b63f83ac5e31a5049
Alessandro Desantis <desa.alessandro@gmail.com>
2018-03-10 13:26:39 +0100

    Fix coding style issues

--- lib/generators/hertz/install_generator.rb
+++ lib/generators/hertz/install_generator.rb
@@ -2,7 +2,7 @@
 
 module Hertz
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('templates', __dir__)
 
     def copy_initializer_file
       copy_file 'initializer.rb', 'config/initializers/hertz.rb'
